5d3750887a44387dd1ba0f28c3738c63ba6b66ed
weilanten <xiaolanlub@outlook.com>
2017-04-04 01:32:55 +0800

    一晚上的时间，wpy版本的主体已经完成 本次完成了：大循环 是否存活和将要死亡的人进行统计并且循环
    接下来要完成：
    * 投票统计
    * 结果公布

--- main.py
+++ main.py
@@ -15,28 +15,114 @@ Witch = ""
 Predictor = ""
 Villager = []
 Scheduled = []
-
+IsAlive = []
+WillKilled = []
+chatroomUserClass = ""
 langrenamout=2
 cunminamout=0
+
+nowGameStatus = 0
 
 @bot.register()
 def print_messages(msg):
     global Player
     global gameStart
+    global WillKilled
     if msg.text == "加入游戏" :
         if gameStart == False:
             if msg.sneder in Player:
                 return "请不要重复加入"
             else:
                 Player.append(msg.sender)
+                msg.sender.send_msg("欢迎加入游戏，您再游戏中的id为"+str(Player.index(msg.sender)+"，加入游戏后不得退出游戏。")
                 if len(Player)>=2+langrenamout+cunminamout:
                     role_distribution()
                     # 开始游戏咯～
+
+    else if msg.text[:5] == "我们决定要杀":
+        if nowGameStatus == 1:
+            if msg.sender in Wolf && IsAlive[Player.index(msg.sender)]:
+                if len(chatroomUserClass.search(msg.text)) != 0:
+                    WillKilled.append(chatroomUserClass.search(msg.text[6:])[0])
+                    nowGameStatus = 2
+                    return "好的我知道了"
+                else:
+                    return "并没有这个人"
+            else:
+                return "你当前的身份不是狼人或者你已经死了，不能发言
+        else:
+            return "当前不是狼人的发言时间。"
+
+    else if msg.text[:1] == "救":
+        if nowGameStatus = 2:
+            if msg.sender == Witch && IsAlive[Player.index(msg.sender)]:
+                if len(chatroomUserClass.search(msg.text)) != 0:
+                    if (chatroomUserClass.search(msg.text[1:])[0] in WillKilled):
+                        WillKilled.remove(chatroomUserClass.search(msg.text[1:])[0])
+                    nowGameStatus = 3
+                    return "好的我知道了"
+                else :
+                    return "游戏中没有这个人"
+            else :
+                return "您当前的游戏身份不是女巫或你处于死亡观战状态"
+        else :
+            return "现在不是女巫发言。"
+
+    else if msg.text[:1] == "毒":
+        if nowGameStatus == 2:
+            if msg.sender == Witch && IsAlive[Player.index(msg.sender)]:
+                if len(chatRoomUserClass.search(msg.text)) != 0:
+                    if (chatroomUserClass.search(msg.text[1:])[0] not in WillKilled):
+                        WillKilled.append(chatroomUserClass.search(msg.text[2:])[0])
+                    nowGameStatus = 3
+                    return "好的我知道了"
+                else :
+                    return "游戏中没有这个人"
+            else :
+                return "您当前的游戏身份不是女巫或你处于死亡观战状态"
+        else :
+            return "现在不是女巫发言。"
+
+    else if msg.text[:3] == "我想知道":
+        if nowGameStatus == 3:
+            if msg.sender == Predictor && . IsAlive[Player.index(msg.sender)]:
+                if len(chatroomUserClass.serach(msg.text[4:])) != 0:
+                    if (chatroomUserClass.serach(msg.text[4:])[0] in Wolf):
+                        return "没错，她（他）是狼人"
+                    else if (chatroomUserClass.serach(msg.text[4:])[0] == Witch):
+                        return "她（他）是女巫"
+                    else if (chatroomUserClass,serach(msg.text[4L])[0] == Predictor):
+                        return "她（他）是你自己 = ="
+                    else if (chatroomUserClass.serach(msg.text[4:])[0] in Villager):
+                        return "她（他）是村民"
+                    nowGameStatus = 4
+                else:
+                    return "对不起，没有这个人"
+            else:
+                return "您不是预言家活着已经死亡"
+        else:
+            return "当前不是预言家的提问环节。"
+
+@bot.register(Group, TEXT)
+def qunneifayan(msg):
+    if msg.sender in WillKilled:
+        msg.reply("好的，谢谢你的观点。")
+        WillKilled.remove(msg.sender)
+        IsAlive[Player.index(msg.sender)] = False
+
+                    
+
+
+
+    
+
+        
+
             
                 
 
 def role_distribution():
-    global Wolf,Predictor,Villager,Scheduled,chatroomUserClass,langrenChatRoomClass
+    global Wolf,Predictor,Villager,Scheduled,chatroomUserClass,langrenChatRoomClass,IsAlive
     Wolf = random.sample(Player)
     for wolf in Wolf :
         wolfMsg = "你的身份是「狼人」，你的同伙为："
@@ -64,7 +150,53 @@ def role_distribution():
             player.send_msg("您的身份是「平民」")
             Scheduled.append(Villager)
             chatroomUserClass = bot.create_group(player,"狼人杀Beta")
-            chatroomUserClass.send_msg("所有的身份已经发放完毕，如有出现没有收到身份等异常翻车情况，请及时私聊。")
+            chatroomUserClass.send_msg("所有的身份已经发放完毕，如有出现没有收到身份等异常翻车情况，请及时私聊，如没毛病，5秒钟后将进入游戏")
+            time.sleep(5000)
+            
+            for i in range(0,len(Player)-1):
+                IsAlive[i] = True
+            main_loop()
+
+def main_loop():
+
+    chatroomUserClass.send_msg("新的一个晚上。天黑请闭眼。")
+    # 游戏大循环
+    if gameStart == True:
+        nowGameStatus = 1
+        chatroomUserClass.send_msg("先请狼人出没，在狼人专用聊天群中交流要杀的人，并请其中一人将要杀的以「我们决定要杀+昵称的格式私聊给我。")
+        while nowGameStatus == 1:
+            wait = True
+            # 卡在这里直到狼人完成发言
+        if (IsAlive[Player.index(Witch)]):
+            chatroomUserClass.send_msg("好的，狼人已经完成了杀人，接着，请女巫睁眼，请问你是要毒人（对我私聊毒+昵称）还是要救人（对我私聊救+昵称）？")
+            while nowGameStatus == 2:
+            wait = True
+            # 日常等待发言
+        if (IsAlive[Player.index(Villager)]):
+            chatroomUserClass.send_msg("好的，那么女巫完成了本轮发言，接着，请预言家睁眼，请问你想知道谁的身份？（对我私聊我想知道+昵称）")
+            while nowGameStatus == 3:
+                wait = True
+
+        chatroomUserClass.send_msg("天亮了，昨晚")
+        for willkilled in WillKilled:
+            chatroomUserClass.send_msg(willkilled.remark_name)
+        chatroomUserClass.send_msg("被杀，请留遗言。")
+        
+        for willkilled in WillKilled:
+            chatroomUserClass.sned_msg("下面，请"+willkilled.remark_name+"做出表态。")
+            while willkilled in WillKilled:
+                wait = True
+                # 直到发言之后。。。
+
+        chatroomUserClass.send_msg("下面，是最激动人心的投票观点，请给位发送投票+昵称的方式投票。")
+
+        
+        
+        
+    '''
+    游戏大循环
+    '''
+    print("GAME START")
             
 
 
