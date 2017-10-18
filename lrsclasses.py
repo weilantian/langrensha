53f527b2a4b72882ad2dff7108eabec892cc920f
yyjlincoln <yyjlincoln@gmail.com>
2017-10-18 19:09:45 +1100

    角色操作文件,已模块化

---
+++ lrsclasses.py
@@ -0,0 +1,136 @@
+#lrsclasses.py
+Langrenx=[]#狼人列表
+Nvwux=[]#女巫列表
+Yuyanjiax=[]#预言家列表
+Pingminx=[]#平民
+Alive=True
+Dead=False
+Players={}
+NvwuChance=1
+class GetError:
+    Error='None'
+    IDto=0
+class Langren:
+    '狼人'
+    global Langrenx
+    number=0
+    def delete(name):
+        if name in Langrenx:
+            Langrenx.remove(name)
+            return True
+        else:
+            GetError.Error='删除时出错:名称不存在'
+            GetError.IDto=GetError.IDto+1
+            return False
+    def new(name):
+        if name in Langrenx:
+            GetError.Error='创建时出错:名称已存在'
+            GetError.IDto=GetError.IDto+1
+            return False
+        else:
+            Langrenx.append(name)
+            return True
+    def kill(name):
+        if Players[name]==Alive:
+                Players[name]==Dead
+                return True
+        else:
+            GetError.Error='尝试写入时出错:目标玩家已死'
+            GetError.IDto=GetError.IDto+1
+            return False    
+class Nvwu:
+    '女巫'
+    global Nvwux
+    number=0
+    def delete(name):
+        if name in Nvwux:
+            Nvwux.remove(name)
+            return True
+        else:
+            GetError.Error='删除时出错:名称不存在'
+            GetError.IDto=GetError.IDto+1
+            return False
+    def new(name):
+        if name in Nvwux:
+            GetError.Error='创建时出错:名称已存在'
+            GetError.IDto=GetError.IDto+1
+            return False
+        else:
+            Nvwux.append(name)
+            return True
+    def kill(name):
+        #什么都没干
+        return True
+    def save(name):
+        if NvwuChance==0:
+            GetError.Error='写入时出错:女巫机会已用完'
+            GetError.IDto=GetError.IDto+1
+            return False
+        if name in Players:
+            if Players[name]==Dead:
+                Players[name]=Alive
+                return True
+            else:
+                GetError.Error='写入时出错:玩家还是活的,不需要救'
+                GetError.IDto=GetError.IDto+1
+                return False
+class Yuuyanjia:
+    '预言家'
+    def delete(name):
+        if name in Yuyanjiax:
+            Yuyanjiax.remove(name)
+            return True
+        else:
+            GetError.Error='删除时出错:名称不存在'
+            GetError.IDto=GetError.IDto+1
+            return False
+    def new(name):
+        if name in Yuyanjiax:
+            GetError.Error='创建时出错:名称已存在'
+            GetError.IDto=GetError.IDto+1
+            return False
+        else:
+            Yuyanjiax.append(name)
+            return True
+    def know(name):
+        return All.job(name)
+class Pingmin:
+    '平民,无权限'
+    def delete(name):
+        if name in Pingminx:
+            Pingminx.remove(name)
+            return True
+        else:
+            GetError.Error='删除时出错:名称不存在'
+            GetError.IDto=GetError.IDto+1
+            return False
+    def new(name):
+        if name in Pingminx:
+            GetError.Error='创建时出错:名称已存在'
+            GetError.IDto=GetError.IDto+1
+            return False
+        else:
+            Pingminx.append(name)
+            return True
+class All:
+    '全体玩家控制'
+    global Players
+    def job(name):
+        if name not in Players:
+            GetError.Error='获取成员职务时出错:名称不存在'
+            GetError.IDto=GetError.IDto+1
+            return False
+        else:
+            if name in Langrenx:
+                return '狼人'
+            elif name in Yuyanjiax:
+                return '预言家'
+            elif name in Pingminx:
+                return '平民'
+            elif name in Nvwux:
+                return '女巫'
+    def alive(name):
+        if name in Players:
+            return Players[name]
+        else:
+            return -1
